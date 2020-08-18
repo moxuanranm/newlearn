e3ce14a29e61da2493f766397c084aa215ee0fe0
Gao Fu <moxaunranm@gmail.com>
2020-08-18 12:41:36 +0800

    instance

---
+++ instance.py
@@ -0,0 +1,23 @@
+# /usr/bin/env python3
+# -*- coding utf-8 -*-
+
+class Student(object):
+
+    name = 'Student'
+
+
+s = Student()
+
+print(s.name)
+
+print(Student.name)
+
+s.name = 'Michael'
+
+print(s.name)
+
+print(Student.name)
+
+del s.name
+
+print(s.name)
