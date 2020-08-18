dfa36e8377e3a8c4d2dc788059de2354cd49815a
Gao Fu <moxaunranm@gmail.com>
2020-08-18 11:07:45 +0800

    private variable

--- class.py
+++ class.py
@@ -5,25 +5,34 @@ class Student(object):
     
     
     def __init__(self,name,score):
-        self.name = name 
-        self.score = score
+        self.__name = name 
+        self.__score = score
+    
     
     def print_score(self):
-        print('%s : %s' % (self.name,self.score))
+        print('%s : %s' % (self.__name,self.__score))
     
     def get_grade(self):
-        if self.score >= 90:
+        if self.__score >= 90:
             return 'A'
-        elif self.score >= 60:
+        elif self.__score >= 60:
             return 'B'
         else:
             return 'C'
 
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+
+
 bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 
-print('bart.name = ',bart.name)
-print('bart.score = ',bart.score)
+print('bart.name = ',bart.get_name())
+print('bart.score = ',bart.get_score())
 bart.print_score()
 
 print('grade of Bart: ',bart.get_grade())
