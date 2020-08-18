a49abfdd6eed4ff4ac9559819925f5fc22548a7b
Gao Fu <moxaunranm@gmail.com>
2020-08-18 12:29:30 +0800

    new heir

---
+++ animal.py
@@ -0,0 +1,35 @@
+# /usr/bin/env python3
+# -*- coding utf-8 -*-
+
+class Animal(object):
+    
+    def run(self):
+        print('Animal is running')
+
+class Dog(Animal):
+    
+    def run(self):
+        print('Dog is running')
+    
+    def eat(self):
+        print('Eating meat')
+
+
+class Cat(Animal):
+    
+    def run(self):
+        print('Cat is running')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+dog = Dog()
+dog.run()
+
+cat = Cat()
+cat.run()
+
+run_twice(Animal())
+run_twice(Dog())
+run_twice(Cat())
