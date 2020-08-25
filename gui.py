03ce2d2a9383ad06fe726c00a16a3e608a6a6205
Gao Fu <moxaunranm@gmail.com>
2020-08-26 07:57:32 +0800

    all the files recently

---
+++ gui.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self,master = None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alterButton = Button(self,text = 'Hello',command = self.hello)
+        self.alterButton.pack()
+    
+    def hello(self):
+        name = self.nameInput.get() or 'world!'
+        messagebox.showinfo('message','Hello, %s' % name)
+        
+
+app = Application()
+app.master.title('Hello, world!')
+app.mainloop()
+
---
+++ solve.py
@@ -0,0 +1,9 @@
+bicycles = []
+
+bicycles.append('honda')
+bicycles.append('yamaha')
+bicycles.append('suzuki')
+
+bicycles.insert(0,'hhh')
+
+print(bicycles)
