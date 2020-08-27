ad3e29ef7dc36b89259eab724cefec6a30cd5c08
Gao Fu <moxaunranm@gmail.com>
2020-08-27 16:44:16 +0800

    first part to develop alien invasion

---
+++ alien_invasion/settings.py
@@ -0,0 +1,12 @@
+class Settings():
+# 储存《外星人入侵》的所有设置类
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
